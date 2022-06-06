741a919372be253a92172015746fa0aaefdce645
James Etchells <etch.rush@gmail.com>
2022-06-06 16:22:59 -0700

    Add item create form view, route, and controller function

--- routes/inventory.js
+++ routes/inventory.js
@@ -8,11 +8,18 @@ var category_controller = require('../controllers/categoryController');
 // GET home page.
 router.get('/', item_controller.index);
 
+// Get request for Item create form.
+router.get('/item/create', item_controller.item_create_get);
+
+// POST request for creating Author.
+
 // Get request for one Item.
 router.get('/item/:id', item_controller.item_detail);
 
 // Get request for one Category
 router.get('/category/:id', category_controller.category_detail);
+
+
 
 
 module.exports = router;
